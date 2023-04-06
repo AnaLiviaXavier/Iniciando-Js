79e065934716448212ad186548b34ee6e136f2a8
Ana Xavier <analiviaxavier333@gmail.com>
2023-04-05 21:58:06 -0300

    Iniciando com JS

---
+++ script.js
@@ -0,0 +1,26 @@
+// isto é um comentario
+
+/* isto também é 
+um comentario */
+
+// console.log('Olá Mundo!');
+
+// prompt("Qual a sua idade?");
+
+// conhecendo e trabalhando com variaveis
+
+/*const diaDeNascimento = 23;
+let texto = 'Meu dia de nascimeto é 29';
+console.log(diaDeNascimento);
+console.log(texto);*/
+
+// concatenando variaveis
+
+//console.log(texto + diaDeNascimento);
+
+
+const primeiroNome = "Fulano"; 
+const sobrenome = 'da Silva';
+let idade = '33';
+let souEstudante = false;
+console.log('Olá, meu nome é', primeiroNome + ' ' + sobrenome + ', tenho ' + idade + ', estou estudando: ' +souEstudante);
